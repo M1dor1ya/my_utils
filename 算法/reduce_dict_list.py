ea2df8e2182ef2fec36b40d5b03eacd9d06b8db5
Zcs <772987650@qq.com>
2019-05-22 17:35:22 +0800

    update

--- 算法/reduce_dict_list.py
+++ 算法/reduce_dict_list.py
@@ -2,13 +2,19 @@ def reduce_list_mem(dict_obj):
     """
     此函数用于多重字典列表的数据，当列表元素过多时，无法很好的查看数据结构信息
     所以递归删除字典中所有的列表元素，只保留第一个元素，更易查看数据结构信息
-	存在的问题：如果是列表嵌套字典再嵌套列表，那么将无法检测到，也无法剔除元素
     :param dict_obj:传入一个dict
     :return:返回一个经过递归删除列表元素的dict
     """
     for key,value in dict_obj.items():
         if type(value) is list:
-            dict_obj.update({key:value[0:1]})
+            after_list = []
+            for i in value[:5]:
+                if type(i) is dict:
+                    after = reduce_list_mem(i)
+                    after_list.append(after)
+                else:
+                    after_list.append(i)
+            dict_obj.update({key:after_list})
         if type(value) is dict:
             after = reduce_list_mem(value)
             dict_obj.update({key:after})
